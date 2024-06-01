c0fe986db8693433421b0ab98e29fa477c0980d8
Sanskar Rawat <rawatavi.211@gmail.com>
2024-06-01 13:32:12 +0000

    "jnlq: M1"

--- src/main/java/com/jnlqcli/JnlqCliCommand.java
+++ src/main/java/com/jnlqcli/JnlqCliCommand.java
@@ -1,13 +1,16 @@
 package com.jnlqcli;
 
+import com.jnlqcli.commands.filter.JsonFilterCommand;
 import com.jnlqcli.commands.importer.JsonImportCommand;
+import com.jnlqcli.commands.search.JsonSearchCommand;
+import com.jnlqcli.commands.sort.JsonSortCommand;
 import io.micronaut.configuration.picocli.PicocliRunner;
 
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 @Command(name = "jnlq-cli", description = "...",
-        mixinStandardHelpOptions = true,subcommands = {JsonImportCommand.class})
+        mixinStandardHelpOptions = true,subcommands = {JsonImportCommand.class, JsonFilterCommand.class, JsonSortCommand.class, JsonSearchCommand.class})
 public class JnlqCliCommand implements Runnable {
 
     @Option(names = {"-v", "--verbose"}, description = "...")
