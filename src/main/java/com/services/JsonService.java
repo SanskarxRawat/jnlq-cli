60fef5060ae9ae4f35b0038dd46e5b2a267f9a8c
Sanskar Rawat <rawatavi.211@gmail.com>
2024-06-01 05:33:12 +0000

    test commit

--- src/main/java/com/services/JsonService.java
+++ src/main/java/com/services/JsonService.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.io.IOException;
 
 public interface JsonService {
+
     void importJson(String filePath) throws IOException;
+
     JsonNode getImportedJson();
 }
